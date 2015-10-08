731c0653c788dbc27c4307bd2af2beb4e0dddd7e
Mike Woelmer <mike.woelmer@atomicobject.com>
2015-10-08 04:33:27 -0400

    Finished swift implementation

--- ComponentSwitch/ViewControllerSwift.swift
+++ ComponentSwitch/ViewControllerSwift.swift
@@ -10,24 +10,55 @@ import UIKit
 
 class ViewControllerSwift: UIViewController {
     @IBOutlet weak var containerView: UIView!
-    weak var currentViewController: UIViewController?;
+    weak var currentViewController: UIViewController?
     
     override func viewDidLoad() {
+        self.currentViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ComponentA")
+        self.currentViewController!.view.translatesAutoresizingMaskIntoConstraints = false
+        self.addChildViewController(self.currentViewController!)
+        self.addSubview(self.currentViewController!.view, toView: self.containerView)
         super.viewDidLoad()
     }
     
-    @IBAction func showComponent(sender: UISegmentedControl) {
-        if sender.selectedSegmentIndex == 0 {
-            UIView.animateWithDuration(0.5, animations: {
-//                self.containerViewA.alpha = 1
-//                self.containerViewB.alpha = 0
-            });
-        } else {
-            UIView.animateWithDuration(0.5, animations: {
-//                self.containerViewA.alpha = 0
-//                self.containerViewB.alpha = 1
-            });
-        }
+@IBAction func showComponent(sender: UISegmentedControl) {
+    if sender.selectedSegmentIndex == 0 {
+        let newViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ComponentA")
+        newViewController!.view.translatesAutoresizingMaskIntoConstraints = false
+        self.cycleFromViewController(self.currentViewController!, toViewController: newViewController!)
+        self.currentViewController = newViewController
+    } else {
+        let newViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ComponentB")
+        newViewController!.view.translatesAutoresizingMaskIntoConstraints = false
+        self.cycleFromViewController(self.currentViewController!, toViewController: newViewController!)
+        self.currentViewController = newViewController
     }
-    
+}
+
+func cycleFromViewController(oldViewController: UIViewController, toViewController newViewController: UIViewController) {
+    oldViewController.willMoveToParentViewController(nil)
+    self.addChildViewController(newViewController)
+    self.addSubview(newViewController.view, toView:self.containerView!)
+    newViewController.view.alpha = 0
+    newViewController.view.layoutIfNeeded()
+    UIView.animateWithDuration(0.5, animations: {
+            newViewController.view.alpha = 1
+            oldViewController.view.alpha = 0
+        },
+        completion: { finished in
+            oldViewController.view.removeFromSuperview()
+            oldViewController.removeFromParentViewController()
+            newViewController.didMoveToParentViewController(self)
+    })
+}
+
+func addSubview(subView:UIView, toView parentView:UIView) {
+    parentView.addSubview(subView)
+
+    var viewBindingsDict = [String: AnyObject]()
+    viewBindingsDict["subView"] = subView
+    parentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[subView]|",
+        options: [], metrics: nil, views: viewBindingsDict))
+    parentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[subView]|",
+        options: [], metrics: nil, views: viewBindingsDict))
+}
 }
